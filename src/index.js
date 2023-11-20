8e8e72b99d2f55f7c70cfc9ceb648c249fd55426
Sean Kipinä <paintballsean@gmail.com>
2023-11-20 15:31:26 +0200

    Added makeImage function and forEach loop for imageData

--- src/index.js
+++ src/index.js
@@ -7,6 +7,18 @@ const imageData = [
   { imageURL: 'https://images.dog.ceo/breeds/mastiff-bull/n02108422_2947.jpg' },
 ]
 
+function makeImage(imgURL) {
+  const image = document.createElement('img');
+  image.src = imgURL.imageURL;
+  image.style.height = '10em'
+  image.classList.add('dog')
+  return image
+}
+
+imageData.forEach(imgObj => { 
+  const imgElement = makeImage(imgObj)
+  // document.body.prepend(imgElement)
+})
 
 // TASK 1- Import the data we need to "hydrate" our component.
 //  On the one hand, the default export from data/panelData.js
