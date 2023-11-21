2d42b427583767ec19625060a0a9abc324385f93
Sean Kipinä <paintballsean@gmail.com>
2023-11-21 12:15:56 +0200

    Addeded testPanel to track changes and completeted task 3.

--- src/index.js
+++ src/index.js
@@ -2,33 +2,35 @@
 //  that takes an { imgURL } and returns an img element.
 //  Then loop over these URLs making images as you go:
 const imageData = [
-  { imageURL: 'https://images.dog.ceo/breeds/mastiff-tibetan/n02108551_978.jpg' },
-  { imageURL: 'https://images.dog.ceo/breeds/mastiff-bull/n02108422_3398.jpg' },
-  { imageURL: 'https://images.dog.ceo/breeds/mastiff-bull/n02108422_2947.jpg' },
-]
+  {
+    imageURL: "https://images.dog.ceo/breeds/mastiff-tibetan/n02108551_978.jpg",
+  },
+  { imageURL: "https://images.dog.ceo/breeds/mastiff-bull/n02108422_3398.jpg" },
+  { imageURL: "https://images.dog.ceo/breeds/mastiff-bull/n02108422_2947.jpg" },
+];
 
 function makeImage(imgURL) {
-  const image = document.createElement('img');
+  const image = document.createElement("img");
   image.src = imgURL.imageURL;
-  image.style.height = '10em'
-  image.classList.add('dog')
-  return image
+  image.style.height = "10em";
+  image.classList.add("dog");
+  return image;
 }
 
-imageData.forEach(imgObj => { 
-  const imgElement = makeImage(imgObj)
+imageData.forEach((imgObj) => {
+  const imgElement = makeImage(imgObj);
   // document.body.prepend(imgElement)
-})
+});
 
 // TASK 1- Import the data we need to "hydrate" our component.
 //  On the one hand, the default export from data/panelData.js
 //  On the other hand, the default export from data/constants.js
 //  Destructure `open` and `close` from the constants
-import panelData from './data/panelData'
-import linkData from './data/linkData'
-import constants from './data/constants'
+import panelData from "./data/panelData";
+import linkData from "./data/linkData";
+import constants from "./data/constants";
 
-const {open, close} = constants
+const { open, close } = constants;
 
 // const open = constraints.open
 // const close = constraints.close
@@ -39,26 +41,21 @@ const {open, close} = constants
 // console.log(open) // log the open arrow
 // console.log(close) // log the close arrow
 
-
 // TASK 3- Comment out the div.panel from index.html and grab its parent element.
 //  We will generate the panel with code, and we'll need the parent
 //  so we can append the code-generated panel to the DOM.
-const accordion = null
-
+const accordion = document.querySelector(".accordion");
 
 // TASK 4- Create a function 'makePanel' that creates a panel exactly as you see it in the HTML.
 function makePanel(/* what data does the panel need? */) {
-
-
   // TASK 5- Instantiate all the elements needed for a panel
-  const panel = null
-  const panelBar = null
-  const panelContent = null
-  const panelTitle = null
-  const panelButtons = null
-  const openButton = null
-  const closeButton = null
-
+  const panel = null;
+  const panelBar = null;
+  const panelContent = null;
+  const panelTitle = null;
+  const panelButtons = null;
+  const openButton = null;
+  const closeButton = null;
 
   // TASK 6- Setup the structure of our elements
   /*
@@ -74,30 +71,26 @@ function makePanel(/* what data does the panel need? */) {
     </div>
   */
 
-
   // TASK 7- Add proper class names to our elements (See index.html for reference)
   // paying attention to the elements that need to start out hidden
 
-
   // TASK 8- Set text content using arguments as raw material
   //  and also using the open and close arrows imported at the top of the file
-
 
   // TASK 9- When the 'open' or 'close' buttons are clicked, the content is toggled on/off:
   //  - the open button needs to go away (the 'hide-btn' class name controls this)
   //  - the close button needs to show (the 'hide-btn' class name controls this)
   //  - the contents need to show (the 'toggle-on' class name controls this)
 
-
   // don't forget to return the panel!
-  return null
+  return null;
 }
-
+const testPanel = makePanel({ title: "foo", content: "bar" });
+accordion.append(testPanel);
 
 // TASK 10- Loop through the panelData we imported from the data folder
 //  creating panels for each content and title and append them to the DOM.
 //  We can do this with a single forEach, or with a map and a forEach.
-
 
 // [STRETCH] Comment out the links inside the nav and
 // write a linkMaker that takes { href, className, text }
